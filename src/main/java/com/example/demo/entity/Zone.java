8be8d9578f19acc3143be5a57798385221e6403c
bala-amypo <bala@amypo.in>
2025-12-18 04:06:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Zone.java
@@ -0,0 +1,27 @@
+package com.example.collectiondb.entity;
+
+public class Zone {
+    private long id;
+    private String zoneName;
+    private Integer  priorityLevel;
+    private Integer population;
+    private Boolean address;
+    public long getId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhno(String phno) {
+        this.phno = phno;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    
+}
