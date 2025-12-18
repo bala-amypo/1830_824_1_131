cad8b00e33f3b650770fa996b7b109a7d8590055
bala-amypo <bala@amypo.in>
2025-12-18 08:25:58 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/SupplyForecastService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.SupplyForecast;
+
+@Service
+public class SupplyForecastService{
+    createForecast(SupplyForecast forecast)
+    updateForecast(Longid,SupplyForecast forecast)
+    
+}   
+
+
+
+
+
+
+studentService.java
