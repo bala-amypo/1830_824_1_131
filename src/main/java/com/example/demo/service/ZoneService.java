ec8cbb56a22ab4578101c71361b5a8d644ee9a5d
bala-amypo <bala@amypo.in>
2025-12-18 08:20:58 +0000

    User commit

--- src/main/java/com/example/demo/service/ZoneService.java
+++ src/main/java/com/example/demo/service/ZoneService.java
@@ -1,1 +1,23 @@
 package com.example.demo.service;
+
+
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Zone;
+
+@Service
+public class ZoneService  {
+    createZone(Zone zone)
+    updateZone(Long id,Zone zone)
+    getZoneById(Long id)
+    getAllZones()
+    deactivateZone(Long id)
+    
+}
+
+
+
+
+
+
