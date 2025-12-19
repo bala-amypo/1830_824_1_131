cbc54bc6f0a0a721ee77ed38f42335afb344f7d5
bala-amypo <bala@amypo.in>
2025-12-19 15:22:20 +0000

    User commit

--- src/main/java/com/example/demo/repository/ZoneRestorationRecordRepository.java
+++ src/main/java/com/example/demo/repository/ZoneRestorationRecordRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.entity.ZoneRestorationRecordRepository;
 
 @Repository
-public interface ZoneRestorationRecordRepository extends JpaRepository<Zone, Long>{
+public interface ZoneRestorationRecordRepository extends JpaRepository<ZoneRestorationRecordRepository , Long>{
 
 }
