82fc4989bea1eb03f7ace5ae46e4558d5c129ead
bala-amypo <bala@amypo.in>
2025-12-19 15:34:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/AppUserRepository.java
+++ src/main/java/com/example/demo/repository/AppUserRepository.java
@@ -3,7 +3,7 @@
 // import org.springframework.data.jpa.repository.JpaRepository;
 // import org.springframework.stereotype.Repository;
 
-// import com.example.demo.entity.AppUserRepository;
+// import com.example.demo.entity.AppUser;
 
 // @Repository
 // public interface  AppUserRepository extends JpaRepository<AppUserRepository, Long>{
--- src/main/java/com/example/demo/repository/LoadSheddingEventRepository.java
+++ src/main/java/com/example/demo/repository/LoadSheddingEventRepository.java
@@ -1,11 +1,11 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-y
-// import com.example.demo.entity.LoadSheddingEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-// @Repository
-// public interface LoadSheddingEventRepository extends JpaRepository<LoadSheddingEventRepository, Long>{
+import com.example.demo.entity.LoadSheddingEvent;
 
-// }
+@Repository
+public interface LoadSheddingEventRepository extends JpaRepository<LoadSheddingEventRepository, Long>{
+
+}
