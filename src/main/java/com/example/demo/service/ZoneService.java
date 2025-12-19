76aec4cfb690a9d1f7f7797fbb611a621f4cbca3
bala-amypo <bala@amypo.in>
2025-12-19 17:02:09 +0000

    User commit

--- src/main/java/com/example/demo/service/ZoneService.java
+++ src/main/java/com/example/demo/service/ZoneService.java
@@ -1,36 +1,36 @@
-// package com.example.demo.service;
+package com.example.demo.service;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 
-// import com.example.demo.entity.Zone;
+import com.example.demo.entity.Zone;
 
-// @Service
-// public class ZoneService {
+@Service
+public class ZoneService {
 
-//     public Zone createZone(Zone zone) {
-//         // TODO: save zone to DB
-//         return zone;
-//     }
+    public Zone createZone(Zone zone) {
+        // TODO: save zone to DB
+        return zone;
+    }
 
-//     public Zone updateZone(Long id, Zone zone) {
-//         // TODO: update zone
-//         return zone;
-//     }
+    public Zone updateZone(Long id, Zone zone) {
+        // TODO: update zone
+        return zone;
+    }
 
-//     public Zone getZoneById(Long id) {
-//         // TODO: fetch zone by id
-//         return new Zone();
-//     }
+    public Zone getZoneById(Long id) {
+        // TODO: fetch zone by id
+        return new Zone();
+    }
 
-//     public List<Zone> getAllZones() {
-//         // TODO: fetch all zones
-//         return List.of();
-//     }
+    public List<Zone> getAllZones() {
+        // TODO: fetch all zones
+        return List.of();
+    }
 
-//     public Zone deactivateZone(Long id) {
-//         // TODO: deactivate zone
-//         return new Zone();
-//     }
-// }
+    public Zone deactivateZone(Long id) {
+        // TODO: deactivate zone
+        return new Zone();
+    }
+}
