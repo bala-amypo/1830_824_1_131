e9e87019eb1cb9a79163db638dba88e0525fe64b
bala-amypo <bala@amypo.in>
2025-12-20 12:14:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/Zone.java
+++ src/main/java/com/example/demo/entity/Zone.java
@@ -11,5 +11,30 @@ public class Zone {
 
     private String name;
 
+    private boolean active = true;
+
     // getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
