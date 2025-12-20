fc44a93e547dcf48940b3f223f1450e9f04b553c
bala-amypo <bala@amypo.in>
2025-12-20 12:07:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/DemandReading.java
+++ src/main/java/com/example/demo/entity/DemandReading.java
@@ -1,62 +1,15 @@
-// package com.example.demo.entity;
+package com.example.demo.entity;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 
-// import java.sql.Timestamp;
+@Entity
+public class DemandReading {
 
-// @Entity
-// public class DemandReading {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
+    private Double reading;
 
-//     private String zone;
-//     private Double demandMW;
-//     private Timestamp recordedAt;
-
-//     public DemandReading() {
-//     }
-
-//     public DemandReading(Long id, String zone, Double demandMW, Timestamp recordedAt) {
-//         this.id = id;
-//         this.zone = zone;
-//         this.demandMW = demandMW;
-//         this.recordedAt = recordedAt;
-//     }
-
-//     public Long getId() {
-//         return id;
-//     }
-
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
-
-//     public String getZone() {
-//         return zone;
-//     }
-
-//     public void setZone(String zone) {
-//         this.zone = zone;
-//     }
-
-//     public Double getDemandMW() {
-//         return demandMW;
-//     }
-
-//     public void setDemandMW(Double demandMW) {
-//         this.demandMW = demandMW;
-//     }
-
-//     public Timestamp getRecordedAt() {
-//         return recordedAt;
-//     }
-
-//     public void setRecordedAt(Timestamp recordedAt) {
-//         this.recordedAt = recordedAt;
-//     }
-// }
+    // getters & setters
+}
