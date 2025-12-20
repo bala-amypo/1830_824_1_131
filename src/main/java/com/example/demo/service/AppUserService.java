b428b44b99b552ee3f61f4cd637ad88342bdc00b
bala-amypo <bala@amypo.in>
2025-12-20 06:07:26 +0000

    User commit

--- src/main/java/com/example/demo/service/AppUserService.java
+++ src/main/java/com/example/demo/service/AppUserService.java
@@ -10,17 +10,17 @@ import com.example.demo.entity.DemandReading;
 public class DemandReadingService {
 
     public DemandReading createReading(DemandReading reading) {
-        // TODO: save reading to DB
+        
         return reading;
     }
 
     public List<DemandReading> getReadingsForZone(Long zoneId) {
-        // TODO: fetch readings for zone
+       
         return List.of();
     }
 
     public DemandReading getLatestReading(Long zoneId) {
-        // TODO: fetch latest reading
+        
         return new DemandReading();
     }
 
