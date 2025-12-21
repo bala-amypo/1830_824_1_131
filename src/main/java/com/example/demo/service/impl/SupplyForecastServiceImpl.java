0bfbee8242b2357e9bc5e181d3e4204c5a0f1d67
bala-amypo <bala@amypo.in>
2025-12-21 05:41:57 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SupplyForecastServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SupplyForecastServiceImpl.java
@@ -10,38 +10,37 @@ import java.util.List;
 @Service
 public class SupplyForecastServiceImpl implements SupplyForecastService {
 
-    private final SupplyForecastRepository SupplyForecastRepository;
+    private final SupplyForecastRepository supplyForecastRepository;
 
-    public SupplyForecastServiceImpl(SupplyForecastRepository SupplyForecastRepository) {
-        this.SupplyForecastRepository = SupplyForecastRepository;
+    public SupplyForecastServiceImpl(SupplyForecastRepository supplyForecastRepository) {
+        this.supplyForecastRepository = supplyForecastRepository;
     }
 
     @Override
     public SupplyForecast createForecast(SupplyForecast forecast) {
-        return SupplyForecastRepository.save(forecast);
+        return supplyForecastRepository.save(forecast);
     }
 
     @Override
     public SupplyForecast updateForecast(Long id, SupplyForecast forecast) {
-        SupplyForecast existing = SupplyForecastRepository.findById(id)
+        SupplyForecast existing = supplyForecastRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Forecast not found"));
-
-        return SupplyForecastRepository.save(existing);
+        return supplyForecastRepository.save(existing);
     }
 
     @Override
     public SupplyForecast getForecastById(Long id) {
-        return SupplyForecastRepository.findById(id)
+        return supplyForecastRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Forecast not found"));
     }
 
     @Override
     public List<SupplyForecast> getAllForecasts() {
-        return SupplyForecastRepository.findAll();
+        return supplyForecastRepository.findAll();
     }
 
     @Override
     public List<SupplyForecast> getLatestForecast() {
-        return SupplyForecastRepository.findAll();
+        return supplyForecastRepository.findAll();
     }
 }
