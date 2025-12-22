30eae9a5453bb5b6a60cab92a479ece53639a6de
bala-amypo <bala@amypo.in>
2025-12-22 08:55:40 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ZoneServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ZoneServiceImpl.java
@@ -19,27 +19,32 @@ public class ZoneServiceImpl implements ZoneService {
 
     @Override
     public Zone createZone(Zone zone) {
-        zone.setactive(true);
-        zone.setcreatedAt(LocalDateTime.now());
-        zone.setupdatedAt(LocalDateTime.now());
+
+        zone.setActive(true);
+        zone.setCreatedAt(LocalDateTime.now());
+        zone.setUpdatedAt(LocalDateTime.now());
+
         return zoneRepository.save(zone);
     }
 
     @Override
     public Zone updateZone(Long id, Zone zone) {
-        Zone existingZone = zoneRepository.findById(id).orElse(null);
 
-        existingZone.setzoneName(zone.getzoneName());
-        existingZone.setpriorityLevel(zone.getpriorityLevel());
-        existingZone.sepopulation(zone.getpopulation());
-        existingZone.setupdatedAt(LocalDateTime.now());
+        Zone existingZone = zoneRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Zone not found"));
+
+        existingZone.setZoneName(zone.getZoneName());
+        existingZone.setPriorityLevel(zone.getPriorityLevel());
+        existingZone.setPopulation(zone.getPopulation());
+        existingZone.setUpdatedAt(LocalDateTime.now());
 
         return zoneRepository.save(existingZone);
     }
 
     @Override
     public Zone getZoneById(Long id) {
-        return zoneRepository.findById(id).orElse(null);
+        return zoneRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Zone not found"));
     }
 
     @Override
@@ -49,9 +54,12 @@ public class ZoneServiceImpl implements ZoneService {
 
     @Override
     public void deactivateZone(Long id) {
-        Zone zone = zoneRepository.findById(id).orElse(null);
-        zone.setactive(false);
-        zone.setupdatedAt(LocalDateTime.now());
+        Zone zone = zoneRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Zone not found"));
+
+        zone.setActive(false);
+        zone.setUpdatedAt(LocalDateTime.now());
+
         zoneRepository.save(zone);
     }
 }
