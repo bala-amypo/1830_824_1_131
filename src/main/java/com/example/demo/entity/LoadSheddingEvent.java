4e60dfde5538a9b44e8c46667198bd6fa60db12c
bala-amypo <bala@amypo.in>
2025-12-22 00:32:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoadSheddingEvent.java
+++ src/main/java/com/example/demo/entity/LoadSheddingEvent.java
@@ -1,30 +1,100 @@
-// package com.example.demo.entity;
+package com.example.demo.entity;
 
-// import jakarta.persistence.*;
-// import jakarta.validation.constraints.Min;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-// import java.time.Instant;
+import java.time.LocalDateTime;
 
-// @Entity
-// @Table(name = "load_shedding_events")
-// public class LoadSheddingEvent {
+@Entity
+public class Zone {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//     @ManyToOne(optional = false)
-//     private Zone zone;
+    @Column(unique = true, nullable = false)
+    private String zone;
 
-//     private Instant eventStart;
-//     private Instant eventEnd;
+    private Timestamp eventStart ;
+    private Integer population;
+    private Boolean active;
 
-//     private String reason;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-//     private Long triggeredByForecastId;
+    // -------- Constructors --------
 
-//     @Min(0)
-//     private Double expectedDemandReductionMW;
+    public Zone() {
+    }
 
-    
-// }
+    public Zone(Long id, String zoneName, Integer priorityLevel, Integer population,
+                Boolean active, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.zoneName = zoneName;
+        this.priorityLevel = priorityLevel;
+        this.population = population;
+        this.active = active;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getzoneName() {
+        return zoneName;
+    }
+
+    public void setzoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public Integer getpriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setpriorityLevel(Integer priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+
+    public Integer getpopulation() {
+        return population;
+    }
+
+    public void sepopulation(Integer population) {
+        this.population = population;
+    }
+
+    public Boolean getactive() {
+        return active;
+    }
+
+    public void setactive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getcreatedAt() {
+        return createdAt;
+    }
+
+    public void setcreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getupdatedAt() {
+        return updatedAt;
+    }
+
+    public void setupdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
