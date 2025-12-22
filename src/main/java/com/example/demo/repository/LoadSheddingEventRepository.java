bef2c5cb575ef89e7eefa52bfa23d500806d3ca1
bala-amypo <bala@amypo.in>
2025-12-22 01:05:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/LoadSheddingEventRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.LoadSheddingEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoadSheddingEventRepository extends JpaRepository<LoadSheddingEvent, Long> {
+}
+
+
+
