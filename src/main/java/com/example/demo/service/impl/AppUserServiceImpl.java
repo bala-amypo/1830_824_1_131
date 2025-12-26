d64de05af048d4b346734a468b3584de5a5fd5c4
bala-amypo <bala@amypo.in>
2025-12-26 16:08:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AppUserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AppUserServiceImpl.java
@@ -4,13 +4,20 @@ import com.example.demo.entity.AppUser;
 import com.example.demo.service.AppUserService;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 public class AppUserServiceImpl implements AppUserService {
-    @Override
-public String login(String email, String password) {
-    return "dummy-token";
-}
 
+    @Override
+    public AppUser register(String email, String password, String role) {
+        AppUser user = new AppUser();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRole(role);
+        return user;
+    }
+
+    @Override
+    public String login(String email, String password) {
+        return "dummy-jwt-token";
+    }
 }
