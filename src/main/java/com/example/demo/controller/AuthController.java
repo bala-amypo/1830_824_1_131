09d4c335849946fa60bc3e344bf0ccde38c0f584
bala-amypo <bala@amypo.in>
2025-12-27 02:46:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -4,43 +4,28 @@ import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.entity.AppUser;
 import com.example.demo.service.AppUserService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@Tag(name = "Authentication", description = "APIs for user authentication")
 public class AuthController {
-
-    private final AppUserService userService;
-
-    public AuthController(AppUserService userService) {
-        this.userService = userService;
+    
+    private final AppUserService appUserService;
+    
+    public AuthController(AppUserService appUserService) {
+        this.appUserService = appUserService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<AppUser> register(@RequestBody AuthRequest request) {
-        AppUser user = userService.register(
-                request.getEmail(),
-                request.getPassword(),
-                "ROLE_USER"
-        );
+        AppUser user = appUserService.register(request.getEmail(), request.getPassword(), "USER");
         return ResponseEntity.ok(user);
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-        String token = userService.login(
-    request.getEmail(),
-    request.getPassword()
-);
-
-
-        AuthResponse response = AuthResponse.builder()
-                .token(token)
-                .build();
-
+        AuthResponse response = appUserService.login(request.getEmail(), request.getPassword());
         return ResponseEntity.ok(response);
     }
 }
